fbc20e0ca6cffcef17f37c1e248de7520c9e4a14
Ryan Lee <ryanm.lee11@gmail.com>
2020-04-14 22:08:49 -0400

    Update

--- game.js
+++ game.js
@@ -281,12 +281,12 @@ BRICK_HIT.src = "sounds/brick_hit.mp3";
 
 const soundElement  = document.getElementById("sound");
 
-soundElement.addEventListener("click", audioManager, false);
+soundElement.addEventListener("click", audioManager);
 
 function audioManager(){
     // CHANGE IMAGE SOUND_ON/OFF
     let imgSrc = soundElement.getAttribute("src");
-    let SOUND_IMG = imgSrc == "/images/SOUND_ON.png" ? "/images/SOUND_OFF.png" : "/images/SOUND_ON.png";
+    let SOUND_IMG = imgSrc === "/images/SOUND_ON.png" ? "/images/SOUND_OFF.png" : "/images/SOUND_ON.png";
     
     soundElement.setAttribute("src", SOUND_IMG);
     
